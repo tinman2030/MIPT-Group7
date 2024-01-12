78751ff5ec7b659a86fc20f37cd75491676db78c
Minh Tran <minhtanh126@gmail.com>
2024-01-11 19:40:31 -0500

    did full adder and the ripple adder

--- binary_adder/adder_anim.py
+++ binary_adder/adder_anim.py
@@ -62,7 +62,7 @@ class HalfAdder(Scene):
         
         # Connections
         self.play(
-            Create(Title),
+            Create(Title, run_time = 2),
             Create(input_A,run_time = 2),
             Create(input_B,run_time = 2),
             Create(xor_gate,run_time = 2),
@@ -80,6 +80,8 @@ class HalfAdder(Scene):
             GrowArrow(Arrow(input_A.get_bottom() + [2.5,-0.132,0], and_gate.get_top() + [-.2,0,0], color=WHITE,stroke_width = 1,tip_length = 0.1)),
             GrowArrow(Arrow(input_B.get_bottom() + [2.5,-0.55,0], and_gate.get_top() + [0.2,0,0], color=WHITE,stroke_width = 1,tip_length = 0.1)),
             GrowArrow(Arrow(and_gate.get_bottom(), carry_label.get_top(), color=WHITE,stroke_width = 1,tip_length = 0.1)),
+
+            GrowFromPoint(Line(Title.get_left() + [-0.5,-0.3,0], Title.get_right() + [0.5,-0.3,0], stroke_width = 3,color = YELLOW),Title.get_left() + [-0.5,-0.3,0],run_time = 10),
         )
 
         # transform the whole scene into a box
@@ -95,23 +97,102 @@ class HalfAdder(Scene):
         carry_label = Text("Carry", font_size= 24).shift(4 * RIGHT + 1 * DOWN)
 
         self.play(
-            Wait(5),
             ReplacementTransform(VGroup(*self.mobjects), box, run_time = 2),
-            Write(box2, run_time = 1),
+            Write(box2, run_time = 2),
 
-            Create(or_gate.shift(DOWN + 2.5*RIGHT)),
+            Create(or_gate.shift(DOWN + 2.5*RIGHT),run_time = 2),
 
-            Create(input_A),
-            Create(input_B),
-            Create(carry_in),
-            Create(Title),
+            Create(input_A,run_time = 2),
+            Create(input_B,run_time = 2),
+            Create(carry_in,run_time = 2),
+            Create(Title,run_time = 2),
 
-            Create(sum_label),
-            Create(carry_label),
+            Create(sum_label,run_time = 2),
+            Create(carry_label,run_time = 2),
             
             GrowFromPoint(Line(input_A.get_right(), box.get_left() + [0,0.2,0], color=WHITE, stroke_width=1),input_A.get_right()),
             GrowFromPoint(Line(input_B.get_right(), box.get_left() + [0,-0.2,0], color=WHITE, stroke_width=1),input_B.get_right()),
             GrowFromPoint(Line(box.get_right() + [0,0.2,0], box2.get_left() + [0,0.2,0], color=WHITE, stroke_width=1),box.get_right() + [0,0.2,0]),
             GrowFromPoint(Line(box2.get_right() + [0,0.2,0], sum_label.get_left(), color=WHITE, stroke_width=1),box2.get_right() + [0,0.2,0]),
+            GrowFromPoint(Line(carry_in.get_right(), carry_in.get_right() + [2.2,0,0], stroke_width = 1),carry_in.get_right()),
+            GrowFromPoint(Line(carry_in.get_right() + [2.2,0,0], carry_in.get_right() + [2.2,1.5,0], stroke_width = 1),carry_in.get_right()),
+            GrowFromPoint(Line(carry_in.get_right() + [2.2,1.5,0], box2.get_left() + [0,-0.25,0], stroke_width = 1),carry_in.get_right() + [2.2,1.5,0]),
+            GrowFromPoint(Line(or_gate.get_right(), carry_label.get_left() + [0,-0,0], stroke_width = 1),or_gate.get_right()),
+
+            GrowFromPoint(Line(box.get_right() + [0,-0.2,0], box.get_right() + [0.3,-0.2,0], stroke_width = 1),box.get_right() + [0,-0.2,0]),
+            GrowFromPoint(Line(box.get_right() + [0.3,-0.2,0], or_gate.get_left() + [-2.46,-0.2,0], stroke_width = 1),box.get_right() + [0,-0.2,0]),
+            GrowFromPoint(Line(or_gate.get_left() + [-2.46,-0.2,0], or_gate.get_left() + [0,-0.2,0], stroke_width = 1),box.get_right() + [-2.46,-0.2,0]),
+
+            GrowFromPoint(Line(box2.get_right() + [0,-0.2,0], box2.get_right() + [0.3,-0.2,0], stroke_width = 1),box.get_right() + [0,-0.2,0]),
+            GrowFromPoint(Line(box2.get_right() + [0.3,-0.2,0], box2.get_right() + [0.3,-1.8,0], stroke_width = 1),box.get_right() + [0.3,-0.2,0]),
+            GrowFromPoint(Line(box2.get_right() + [0.3,-1.8,0], box2.get_right() + [0.75,-1.8,0], stroke_width = 1),box.get_right() + [0.75,-1.8,0]),
+
+            GrowFromPoint(Line(Title.get_left() + [-0.5,-0.3,0], Title.get_right() + [0.5,-0.3,0], stroke_width = 3,color = YELLOW),Title.get_left() + [-0.5,-0.3,0],run_time = 10),
+
         )
-        
+        title = Text("3-Bit Ripple Adder", font_size = 30).shift(3*UP)
+        box0 = VGroup(Square(side_length=1, color = BLUE),Text("Full\nAdder", font_size = 20).move_to(Square())).shift(2*RIGHT)
+        box1 = box0.copy().next_to(box0, 1.2 * LEFT, buff = 1)
+        box2 = box0.copy().next_to(box1, 1.2 * LEFT, buff = 1)
+
+        input_A0 = Text("A0", font_size = 18).next_to(box0,UP * 3)
+        input_B0 = Text("B0", font_size = 18).next_to(input_A0,RIGHT * 0.35)
+
+        input_A1 = Text("A1", font_size = 18).next_to(box1,UP * 3)
+        input_B1 = Text("B1", font_size = 18).next_to(input_A1,RIGHT * 0.35)
+
+        input_A2 = Text("A2", font_size = 18).next_to(box2,UP * 3)
+        input_B2 = Text("B2", font_size = 18).next_to(input_A2,RIGHT * 0.35)
+
+        sum0 = Text("Sum0", font_size = 18).next_to(box0,2.5*DOWN)
+        sum1 = Text("Sum1", font_size = 18).next_to(box1,2.5*DOWN)
+        sum2 = Text("Sum2", font_size = 18).next_to(box2,2.5*DOWN)
+
+        c0 = Text("Carry0", font_size = 16).next_to(box0,LEFT)
+        c1 = Text("Carry1", font_size = 16).next_to(box1,LEFT)
+
+        carry_out = Text("Carry\nOut", font_size = 18).next_to(sum2,3 * LEFT)
+
+        self.play(
+            Create(title, run_time = 2),
+
+            ReplacementTransform(VGroup(*self.mobjects), box0, run_time = 2),
+            Write(box1, run_time = 2),
+            Write(box2, run_time = 2),
+
+            Create(input_A0),
+            Create(input_B0),
+
+            Create(input_A1),
+            Create(input_B1),
+
+            Create(input_A2),
+            Create(input_B2),
+
+            Create(sum0),
+            Create(sum1),
+            Create(sum2),
+
+            Create(c0),
+            Create(c1),
+            Create(carry_out),
+
+            GrowFromPoint(Line(input_A0.get_bottom(), box0.get_top(), color=WHITE, stroke_width=1),input_A0.get_bottom()),
+            GrowFromPoint(Line(input_B0.get_bottom(), box0.get_top() + [0.37,0,0], color=WHITE, stroke_width=1),input_B0.get_bottom()),
+            GrowFromPoint(Line(input_A1.get_bottom(), box1.get_top(), color=WHITE, stroke_width=1),input_A1.get_bottom()),
+            GrowFromPoint(Line(input_B1.get_bottom(), box1.get_top() + [0.37,0,0], color=WHITE, stroke_width=1),input_B1.get_bottom()),
+            GrowFromPoint(Line(input_A2.get_bottom(), box2.get_top(), color=WHITE, stroke_width=1),input_A2.get_bottom()),
+            GrowFromPoint(Line(input_B2.get_bottom(), box2.get_top() + [0.37,0,0], color=WHITE, stroke_width=1),input_B2.get_bottom()),
+
+            GrowFromPoint(Line(box0.get_bottom(), sum0.get_top(), color=WHITE, stroke_width=1),box0.get_bottom()),
+            GrowFromPoint(Line(box1.get_bottom(), sum1.get_top(), color=WHITE, stroke_width=1),box1.get_bottom()),
+            GrowFromPoint(Line(box2.get_bottom(), sum2.get_top(), color=WHITE, stroke_width=1),box2.get_bottom()),
+
+            GrowFromPoint(Line(box0.get_left() + [0,-0.1,0], box1.get_right() + [0,-0.1,0], color=WHITE, stroke_width=1),box0.get_left() + [0,-0.1,0]),
+            GrowFromPoint(Line(box1.get_left() + [0,-0.1,0], box2.get_right() + [0,-0.1,0], color=WHITE, stroke_width=1),box1.get_left() + [0,-0.1,0]),
+
+            GrowFromPoint(Line(box2.get_left() + [0,-0.1,0], carry_out.get_top() + [0,0.9,0], color=WHITE, stroke_width=1),box1.get_left() + [0,-0.1,0]),
+            GrowFromPoint(Line(carry_out.get_top(), carry_out.get_top() + [0,0.9,0], color=WHITE, stroke_width=1),carry_out.get_top()),
+
+            GrowFromPoint(Line(title.get_left() + [-0.5,-0.3,0], title.get_right() + [0.5,-0.3,0], stroke_width = 3,color = YELLOW),title.get_left() + [-0.5,-0.3,0],run_time = 10),
+        )
