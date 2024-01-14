6632a2092e4ab96b93b3a18f19af901f75496381
Minh Tran <minhtanh126@gmail.com>
2024-01-13 21:47:25 -0500

    Made the the n-bit ripple adder scene

--- scrapyard.py
+++ scrapyard.py
@@ -1,26 +1,22 @@
 from manim import *
 from itertools import combinations
 
-class MakeAdder(Scene):
+class MakeAdder(MovingCameraScene):
     def construct(self):
-        boxes_group = BoxesGroup()
-        self.play(Create(boxes_group), run_time = 5)
-        self.wait(1)
+        num_units = 8
 
-class BoxesGroup(VGroup):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.create_boxes()
+        adder_units = VGroup(*[AdderUnit().shift((LEFT * 2.2) * i) for i in range(num_units)])
 
-    def create_boxes(self):
-        for i in range(10):
-            box = Square(side_length=1, color=BLUE)
-            text = Text(f"Box {i}", font_size=12)
-            group = VGroup(box, text).arrange(DOWN)
-            self.add(group)
+        center_of_units = adder_units.get_center()
 
-        # Align the boxes in a row
-        self.arrange(RIGHT, buff=0.5)
+        # Set the camera to focus on the center of the units
+        self.camera.frame.set_width(adder_units.width * 1.4)
+        self.camera.frame.move_to(center_of_units)
+
+        # Display the adder units
+        self.play(Create(adder_units, run_time = num_units))
+        self.wait(2) 
+
 class BinaryConversion(Scene):
     def construct(self, original_list = [1,2,3,4,5]):
         # Display the original list
@@ -47,7 +43,41 @@ class BinaryConversion(Scene):
         for i in range(len(combo)):
             sublist_text = Text(f"{combo[i]}", font_size=18).next_to(shit, DOWN*(i+1))
             self.play(Write(sublist_text), run_time=1)
-            self.wait(0.8)
+            self.wait(0.5)
+class AdderUnit(VGroup):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.createUnit()
+    def construct(self):
+        # Create an instance of AdderUnit
+        adder_unit = AdderUnit()
+
+        # Add it to the scene
+        self.add(adder_unit)
+
+        # Play animations
+        self.play(
+            Create(adder_unit),  # You can use your own animations here
+            run_time=2
+        )
+
+        # Wait for a moment
+        self.wait(1)
+
+    def createUnit(self):
+        box = VGroup(Square(side_length=1, color = BLUE),Text("Full\nAdder", font_size = 20).move_to(Square())).shift(2*RIGHT)
+        input_A = Text("A0", font_size = 18).next_to(box,UP * 3)
+        input_B = Text("B0", font_size = 18).next_to(input_A,RIGHT * 0.35)
+        sum = Text("S0", font_size = 18).next_to(box,2.5*DOWN)
+        c = Text("C0", font_size = 16).next_to(box,LEFT)
+
+        line_A = Line(input_A.get_bottom(), box.get_top(), color=WHITE, stroke_width=1)
+        line_B = Line(input_B.get_bottom(), box.get_top() + [0.37, 0, 0], color=WHITE, stroke_width=1)
+        line_sum = Line(box.get_bottom(), sum.get_top(), color=WHITE, stroke_width=1)
+        line_carry = Line(box.get_left() + [0, -0.1, 0], box.get_left() + [-1.2, -0.1, 0], color=WHITE, stroke_width=1)
+
+
+        self.add(box, input_A, input_B, sum, c, line_A, line_B, line_sum, line_carry)
 
     def decimal_to_binary_list(self, decimal_list):
         return [bin(x)[2:] for x in decimal_list]
